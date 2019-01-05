090ae838f312e2b6409b63ef70c9ac18d9097627
joaoDragado <dubmecoco@yahoo.com>
2019-01-05 19:49:56 +0200

    Moved various functions from HW2 notebook to hw2.py.

--- projects/SimulatingSilver/hw2.py
+++ projects/SimulatingSilver/hw2.py
@@ -1,6 +1,10 @@
 from collections import defaultdict
 import json
 
+import numpy as np
+import pandas as pd
+from scipy.special import erf
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -117,7 +121,6 @@ with open("../data/hw2_data/us-states.json") as json_data:
                 state2poly[state].extend(polygon)
 
 
-
 def draw_state(plot, stateid, **kwargs):
     """
     draw_state(plot, stateid, color=..., **kwargs)
@@ -130,7 +133,6 @@ def draw_state(plot, stateid, **kwargs):
     for polygon in state2poly[stateid]:
         xs, ys = zip(*polygon)
         plot.fill(xs, ys, **kwargs)
-
 
 
 def make_map(states, label):
@@ -161,7 +163,7 @@ def make_map(states, label):
       if state in skip:
           continue
       color = cmap(norm(states.ix[state]))
-      draw_state(ax, state, color = color, ec='k')
+      draw_state(ax, state, fc=color, ec='k')
   #add an inset colorbar
   ax1 = fig.add_axes([0.45, 0.70, 0.4, 0.02])
   cb1=mpl.colorbar.ColorbarBase(ax1, cmap=cmap,
@@ -174,3 +176,116 @@ def make_map(states, label):
   ax.set_xlim(-180, -60)
   ax.set_ylim(15, 75)
   return ax
+
+
+
+def simulate_election(model, n_sim):
+    '''
+    args :
+      model (df) : summarizes an election forecast ; 51 rows, indexed by state.
+        cols: Obama prob Obama wins the state,
+        Votes : Electoral votes awarded
+      n_sim (int) : Number of simulations to run
+    returns : Numpy array with n_sim elements, each holds no of votes Obama wins.
+    '''
+    # each col simulates a single outcome from the 50 states + DC
+    # Obama wins the simulation if
+    # the random number is < the win probability
+    simulations = np.random.uniform(size=(51, n_sim))
+    obama_votes = (simulations <
+                   model.Obama.values.reshape(-1, 1)
+                  ) * model.Votes.values.reshape(-1, 1)
+    # summing over rows gives the total electoral votes
+    # for each simulation
+    return obama_votes.sum(axis=0)
+
+
+def plot_simulation(simulation):
+    '''
+    args :
+      simulation: mumpy array with n_sim (see simulate_election) elements
+      Each element stores the number of electoral college votes
+      Obama wins in each simulation.
+    '''
+    plt.hist(simulation, bins=np.arange(240, 375, 1),
+             label='simulations', align='left', density=True)
+    plt.axvline(332, 0, .5, color='r',
+                linestyle='--', label='Actual Outcome')
+    plt.axvline(269, 0, .5, color='k',
+                linestyle='--', label='Victory Threshold')
+
+    p05 = np.percentile(simulation, 5.)
+    p95 = np.percentile(simulation, 95.)
+    iq = int(p95 - p05)
+
+    pwin = ((simulation >= 269).mean() * 100)
+
+    plt.title("Chance of Obama Victory: {:.1f}%, Spread: {} votes".format(
+    pwin, iq))
+    plt.legend(frameon=False, loc='upper left')
+    plt.xlabel("Obama Electoral College Votes")
+    plt.ylabel("Probability")
+    remove_border()
+
+
+def simple_gallup_model(gallup):
+    """
+    A simple forecast that predicts an Obama (Democratic) victory with
+    0 or 100% probability, depending on whether a state
+    leans Republican or Democrat.
+    ------
+    args :
+        gallup (DataFrame) : The Gallup dataframe above
+
+    returns :
+        model (DataFrame) :
+        cols :
+            Obama: prob state votes for Obama. binary vals (0 or 1)
+
+        model.index should be set to gallup.index (indexed by state name)
+    """
+    data = ((gallup.Dem_Adv > 0).astype(float))
+    return pd.DataFrame(data=data).rename(columns={'Dem_Adv':'Obama'})
+
+
+def uncertain_gallup_model(gallup):
+    """
+    predicts an Obama (Dem) victory if the random variable drawn
+    from a Gaussian with mean Dem_Adv and standard deviation 3% is >0
+    -------
+    args :
+        gallup (DataFrame) : The Gallup dataframe above
+
+    returns :
+        model (DataFrame) :
+            cols : * Obama: probability that the state votes for Obama.
+        model.index should be set to gallup.index (indexed by state name)
+    """
+    sigma = 3
+    prob = 1 -  .5 * (1 + erf(-1*gallup.Dem_Adv / np.sqrt(2 * sigma**2)))
+
+    return pd.DataFrame(data=prob, index=gallup.index).rename(columns={'Dem_Adv':'Obama'})
+
+
+def biased_gallup(gallup, bias):
+    """
+    Simulates correcting a hypothetical bias towards Democrats
+    in the original Gallup data.
+    Subtracts a fixed amount from Dem_Adv,
+    before computing the uncertain_gallup_model.
+    -------
+    args :
+        gallup (DataFrame) : The Gallup party affiliation dataframe
+        bias (float) : The amount by which to shift each prediction
+
+    returns :
+            model (DataFrame) :
+                cols : * Obama: probability that the state votes for Obama.
+    --Test--
+    >>> model = biased_gallup(gallup, 1.)
+    >>> model.ix['Florida']
+    >>> .460172
+    """
+    g2 = gallup.copy()
+    g2.Dem_Adv -= bias
+    return uncertain_gallup_model(g2)
