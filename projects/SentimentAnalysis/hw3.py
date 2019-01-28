779ab73bd3b4f4d0b0644a171d0d9f6721de6dfa
joaoDragado <dubmecoco@yahoo.com>
2019-01-28 19:22:23 +0200

    datasets & helper script added.

---
+++ projects/SentimentAnalysis/hw3.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.rcsetup import cycler
+
+dark2_colors = [(0.10588235294117647, 0.6196078431372549, 0.4666666666666667),
+                (0.8509803921568627, 0.37254901960784315, 0.00784313725490196),
+                (0.4588235294117647, 0.4392156862745098, 0.7019607843137254),
+                (0.9058823529411765, 0.1607843137254902, 0.5411764705882353),
+                (0.4, 0.6509803921568628, 0.11764705882352941),
+                (0.9019607843137255, 0.6705882352941176, 0.00784313725490196),
+                (0.6509803921568628, 0.4627450980392157, 0.11372549019607843)]
+
+
+from matplotlib.rcsetup import cycler
+
+mpl.rc('figure', figsize=(10,6), dpi=150)
+mpl.rc('axes', facecolor='white',
+       prop_cycle=cycler('color', dark2_colors ))
+mpl.rc('lines', lw=2)
+mpl.rc('patch', ec='white', fc=dark2_colors[0])
+mpl.rc('font', size=14, family='StixGeneral')
+
+
+def remove_border(axes=None, top=False, right=False, left=True, bottom=True):
+    """
+    Minimize chartjunk by stripping out unnecesasry plot borders and axis ticks
+
+    The top/right/left/bottom keywords toggle whether the corresponding plot border is drawn
+    """
+    ax = axes or plt.gca()
+    ax.spines['top'].set_visible(top)
+    ax.spines['right'].set_visible(right)
+    ax.spines['left'].set_visible(left)
+    ax.spines['bottom'].set_visible(bottom)
+
+    #turn off all ticks
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_ticks_position('none')
+
+    #now re-enable visibles
+    if top:
+        ax.xaxis.tick_top()
+    if bottom:
+        ax.xaxis.tick_bottom()
+    if left:
+        ax.yaxis.tick_left()
+    if right:
+        ax.yaxis.tick_right()
+
+
