49e25da4a3383f94c635b08262d7ef0eb45abc22
joaoDragado <dubmecoco@yahoo.com>
2019-01-31 20:07:22 +0200

    Naive Bayes classifier

--- projects/SentimentAnalysis/hw3.py
+++ projects/SentimentAnalysis/hw3.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.rcsetup import cycler
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 dark2_colors = [(0.10588235294117647, 0.6196078431372549, 0.4666666666666667),
                 (0.8509803921568627, 0.37254901960784315, 0.00784313725490196),
@@ -48,3 +50,37 @@ def remove_border(axes=None, top=False, right=False, left=True, bottom=True):
         ax.yaxis.tick_right()
 
 
+def histogram_style():
+    remove_border(left=False)
+    plt.grid(False)
+    plt.grid(axis='y', color='w', linestyle='-', lw=1)
+
+
+def make_xy(critics, vectorizer=None):
+    """
+    Build a bag-of-words training set for the review data
+    - args
+        critics(dataFrame) : The review data
+
+        vectorizer : CountVectorizer object (optional)
+        A CountVectorizer object to use. If None,
+        then create and fit a new CountVectorizer.
+        Otherwise, re-fit the provided CountVectorizer
+        using the critics data
+
+    -returns
+        X : numpy array (dims: nreview, nwords)
+            Bag-of-words representation for each review.
+        Y : numpy array (dims: nreview)
+            1/0 array. 1 = fresh review, 0 = rotten review
+
+    Examples
+    --------
+    X, Y = make_xy(critics)
+    """
+    if vectorizer is None:
+        vectorizer = CountVectorizer(ngram_range=(1, 2),
+                                     token_pattern=r'\b\w+\b')
+    X = vectorizer.fit_transform(critics.quote)
+    Y = (critics.fresh == 'fresh').values.astype(int)
+    return X, Y
